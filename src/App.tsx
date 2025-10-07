895301f1507d99fe83c89d365250474381bae538
Taisei Yokoshima <taiseiyokoshima030712@gmail.com>
2025-10-07 20:16:00 +0100

    now div typer also works with the absolute div measurement aproach

--- src/App.tsx
+++ src/App.tsx
@@ -30,16 +30,16 @@ function App() {
       console.log("signaling title to start animation");
       titleSignalerRef.current();
 
-      console.log("awaiting title animation to finish...")
-      await titlePromiseRef.current;
-      console.log("title animation finished");
+      // console.log("awaiting title animation to finish...")
+      // await titlePromiseRef.current;
+      // console.log("title animation finished");
 
       console.log("signaling divs to start animation");
       divsSignalerRef.current();
 
-      console.log("awaiting divs animation to finish...")
-      await Promise.all(divsPromisesRef.current);
-      console.log("divs animation finished");
+      // console.log("awaiting divs animation to finish...")
+      // await Promise.all(divsPromisesRef.current);
+      // console.log("divs animation finished");
    };
 
 
@@ -47,9 +47,9 @@ function App() {
       <>
          <div id='terminal-window'>
 
-            <Title signal={titleSignalRef} resolver={titleResolverRef}> this is the title</Title>
+            {/* <Title signal={titleSignalRef} resolver={titleResolverRef}> this is the title</Title> */}
 
-            <Div signal={divsSignalRef} promises={divsPromisesRef} speed={100}> this is the first text I want </Div>
+            <Div signal={divsSignalRef} promises={divsPromisesRef} speed={35}> this is the first text I want. I am trying out</Div>
             <Div signal={divsSignalRef} promises={divsPromisesRef}> this is the second text I want </Div>
 
             <button onClick={onClick}> press me </button>
--- src/TypeWriteres/Div.tsx
+++ src/TypeWriteres/Div.tsx
@@ -1,9 +1,6 @@
 import "../App.css";
-
-
 import { useRef, useEffect } from "react";
 import { type TyperProps, clear_interval, createReffedDefferedPromise } from './utils';
-
 
 function get_resolver(promises: React.RefObject<Promise<void>[]> | undefined) {
    if (promises) {
@@ -15,15 +12,27 @@ function get_resolver(promises: React.RefObject<Promise<void>[]> | undefined) {
    return undefined;
 }
 
-
 export default function Div({ children, speed = 30, signal, promises }: TyperProps) {
    const textRef = useRef<HTMLDivElement>(null);
-   let new_char = true;
-   let index = 0;
+   const to_set = useRef<HTMLDivElement>(null);
+   const to_measure = useRef<HTMLDivElement>(null);
+   const has_run = useRef(false);
 
    useEffect(() => {
-      const maybe_resolver = get_resolver(promises);
+      if (has_run.current) return console.log("use effect has already ran for div");
+      has_run.current = true;
 
+      console.log("use effect ran");
+      if (to_measure.current && to_set.current) {
+         const {width, height} = to_measure.current.getBoundingClientRect();
+         to_set.current.style.minWidth = `${width}px`;
+         to_set.current.style.minHeight = `${height}px`;
+      }
+
+      let new_char = true;
+      let index = 0;
+
+      const maybe_resolver = get_resolver(promises);
       let interval: number;
       const animate = async () => {
          if (signal) await signal.current;
@@ -39,14 +48,12 @@ export default function Div({ children, speed = 30, signal, promises }: TyperPro
             if (new_char) {
                const code = Math.floor(Math.random() * (126 - 32 + 1)) + 32;
                const char = String.fromCharCode(code);
-               const old_text = textRef.current.textContent;
-               const new_text = old_text.substring(0, index) + char + old_text.substring(index + 1);
-               textRef.current.textContent = new_text;
+               textRef.current.textContent += char;
                new_char = false;
             } else {
                const char = children[index];
                const old_text = textRef.current.textContent;
-               const new_text = old_text.substring(0, index) + char + old_text.substring(index + 1);
+               const new_text = old_text.substring(0, index) + char;
                textRef.current.textContent = new_text;
                index++;
                new_char = true;
@@ -58,11 +65,12 @@ export default function Div({ children, speed = 30, signal, promises }: TyperPro
       return () => clear_interval(interval);
    }, []);
 
-
-
    return (
-      <div style={{ fontFamily: "monospace", fontSize: "20px", whiteSpace: "pre" }} ref={textRef}>
-         {" ".repeat(children.length)}
+      <div>
+         <div style={{ position: "absolute", visibility: "hidden", fontFamily: "monospace", fontSize: "20px", }} ref={to_measure}>{children}</div>
+         <div ref={to_set} style={{ display: "inline-block", verticalAlign: "bottom" }}>
+            <div style={{ fontFamily: "monospace", fontSize: "20px", display: "inline" }} ref={textRef}/>
+         </div>
       </div>
    )
 }
