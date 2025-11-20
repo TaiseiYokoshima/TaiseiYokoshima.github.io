d2d16328d56e50af1e69756b1f2b9dacad73cd3e
Taisei Yokoshima <taiseiyokoshima030712@gmail.com>
2025-11-20 16:22:23 +0000

    temp

--- src/Pages/Settings.tsx
+++ src/Pages/Settings.tsx
@@ -7,9 +7,9 @@ import { toggleSettings, toggleAnimation, toggleDarkMode } from "../store";
 
 
 function Setting({callback, children, enabled }: { callback: () => void, children: string, enabled: boolean }) {
-   return <div>
-      <div style={{display: "inline-block"}}>{`${children}:`}</div>
-      <div onClick={callback} style={{display: "inline-block", marginLeft: '10vw', cursor: 'pointer'}}>{`[${(enabled) ? "x" : " " }]`}</div>
+   return <div style={{ display: 'flex', width: "100%", justifyContent: 'center', }}>
+      <div style={{flex: "0 0 60%",  textAlign: 'right', fontSize: '30px',}}>{`${children}:`}</div>
+      <div onClick={callback} style={{flex: "0 0 40%", fontSize: '30px', textAlign: 'left', paddingLeft: '1%', cursor: 'pointer'}}>{`[${(enabled) ? "x" : " " }]`}</div>
    </div>;
 }
 
@@ -28,8 +28,12 @@ export default function Settings()  {
       { settingsOpen ? 
          <>
             <div className="settings-page terminal">
-               <Setting callback={flipAnimation} enabled={animationEnabled}>Animation Enabled</Setting>
-               <Setting callback={flipDarkMoode} enabled={darkModeEnabled}>Dark Mode Enabled</Setting>
+               <div style={{fontSize: '45px', textAlign: 'center', marginBottom: '2vh'}}>Settings</div>
+               <div onClick={closer} style={{position: 'absolute', right: 0, top: 0, cursor: 'pointer', background: 'white', color: 'black', width: '1.8em', height: '1.8em', display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center'}}>X</div>
+               <div className="container">
+                  <Setting callback={flipAnimation} enabled={animationEnabled}>Animation Enabled</Setting>
+                  <Setting callback={flipDarkMoode} enabled={darkModeEnabled}>Dark Mode Enabled</Setting>
+               </div>
             </div> 
             <div onClick={closer} style={{position: 'absolute', top: 0, left: 0, width: '100vw', height: '100vh', zIndex: 2}}/>
          </> 
