f951051db41830720f3860a33d6189851f671053
Taisei Yokoshima <taiseiyokoshima030712@gmail.com>
2025-11-25 21:43:31 +0000

    added coolors

--- src/NavBar/NavBar.tsx
+++ src/NavBar/NavBar.tsx
@@ -8,24 +8,46 @@ import { useEffect, useRef, useState } from "react";
 
 import Settings from "./Settings";
 
-function MenuOpener({callback}: { callback: () => void}) {
-   return <div onClick={callback} style={{ position: "absolute", left: 0, top: 0, marginTop: '1vh', marginLeft: '1vh', fontSize: '20px', cursor: 'pointer'}}> Menu » </div>;
+function MenuOpener({ opener }: { opener: () => void }) {
+   return <div onClick={opener} className="opener"> Menu » </div>;
 }
 
-function MenuCloser({ callback }: {callback: () => void}) {
-   return <div onClick={callback} className="side-components" >«</div>;
+function MenuCloser({ closer }: { closer: () => void }) {
+   return <div onClick={closer} className="side-components" >«</div>;
 }
 
-
-function SettingsOpener({ callback }: { callback: () => void }) {
-   return <div  onClick={callback} className="side-components" >⚙</div>;
-}
-
-export default function NavBar() {
-   const lastAnimation = useSelector((state: RootState) => state.app.lastAnimation);
+function SettingsOpener() {
    const dispatch = useDispatch();
    const openSettings = () => dispatch(toggleSettings());
+   return <div onClick={openSettings} className="side-components" >⚙</div>;
+}
+
+function NavBarCore({ closeMenuNow, cancelClose }: { closeMenuNow: () => void, cancelClose: () => void }) {
+   return <div className="navbar">
+      <MenuCloser closer={closeMenuNow}/>
+      <div className="pages-section">
+         <PageItem cancelClose={cancelClose}>About</PageItem>
+         <PageItem cancelClose={cancelClose}>Projects</PageItem>
+         <PageItem cancelClose={cancelClose}>Experience</PageItem>
+         <PageItem cancelClose={cancelClose}>Education</PageItem>
+         <PageItem cancelClose={cancelClose}>Contact</PageItem>
+      </div>
+      <SettingsOpener/>
+   </div>;
+}
+
+
+
+
+export default function Navbar() {
+   const lastAnimation = useSelector((state: RootState) => state.app.lastAnimation);
    const timeoutRef = useRef<number | null>(null);
+   const [menuOpened, openMenu, closeMenu] = (() => {
+      const [menuOpened, setter] = useState(false);
+      const openMenu = () => setter(true);
+      const closeMenu = () => setter(false);
+      return [menuOpened, openMenu, closeMenu];
+   })();
 
    useEffect(() => {
       if (lastAnimation === 'open') {
@@ -33,12 +55,6 @@ export default function NavBar() {
       };
    }, [lastAnimation]);
 
-   const [ menuOpened, openMenu, closeMenu ] = (() => { 
-      const [menuOpened, setter] = useState(false);
-      const openMenu = () => setter(true);
-      const closeMenu = () => setter(false);
-      return [menuOpened, openMenu, closeMenu ];
-   })();
 
    const cancelClose = () => {
       if (timeoutRef.current) clearTimeout(timeoutRef.current);
@@ -52,7 +68,7 @@ export default function NavBar() {
    };
 
 
-   const closeMenuNow = () => { 
+   const closeMenuNow = () => {
       cancelClose();
       closeMenu();
    };
@@ -62,21 +78,13 @@ export default function NavBar() {
       scheduleClose();
    };
 
+
+   const BarOrOpener = (menuOpened) ? <NavBarCore cancelClose={cancelClose} closeMenuNow={closeMenuNow}/> : <MenuOpener opener={openMenuNow}/>;
    return <>
-      { (menuOpened) ? 
-         <div className="navbar">
-            <MenuCloser callback={closeMenuNow}/>
-            <div className="pages-section">
-               <PageItem cancelClose={cancelClose}>About</PageItem>
-               <PageItem cancelClose={cancelClose}>Projects</PageItem>
-               <PageItem cancelClose={cancelClose}>Experience</PageItem>
-               <PageItem cancelClose={cancelClose}>Education</PageItem>
-               <PageItem cancelClose={cancelClose}>Contact</PageItem>
-            </div>
-            <SettingsOpener callback={openSettings}/>
-         </div>
-         : <MenuOpener callback={openMenuNow}/> 
-      }
+      { BarOrOpener }
       <Settings/>
    </>;
+
+
+
 }
