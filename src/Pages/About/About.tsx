381b874a0929f7569124c567242b4beefa9e8c66
Taisei Yokoshima <taiseiyokoshima030712@gmail.com>
2025-12-11 21:56:29 +0000

    nothing much

--- src/Pages/About/About.tsx
+++ src/Pages/About/About.tsx
@@ -1,6 +1,6 @@
 import { Header, Div } from "../../TypeWriteres";
 
-
+const test_string = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 
 function Text() {
    return <Div>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Div>;
@@ -8,7 +8,7 @@ function Text() {
 
 export default function About() {
    return <>
-         <Header>Header test</Header>
+         <Header speed={1}>{test_string}</Header>
          <Text/>
          <Text/>
          <Text/>
