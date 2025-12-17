c6daf9b981d833395759baf71e17a3a86660d99d
Taisei Yokoshima <taiseiyokoshima030712@gmail.com>
2025-12-17 18:30:35 +0000

    added github and made the links and emails clickable

--- src/Pages/Contact/Contact.tsx
+++ src/Pages/Contact/Contact.tsx
@@ -1,24 +1,30 @@
 import { Div, Header } from "../../TypeWriteres";
 
-
 function Email() {
    return <div className="mt-[5rem]!">
       <Header>Email</Header>
-      <Div>taiseiyokoshima.professional@gmail.com</Div>
+      <Div email="taiseiyokoshima.professional.@gmail.com">taiseiyokoshima.professional@gmail.com</Div>
    </div>;
 }
 
 function LinkedIn() {
    return <div className="mt-[5rem]!">
      <Header>LinkedIn</Header>
-     <Div>linkedin.com/in/taisei-yokoshima/</Div>
+     <Div href={'linkedin.com/in/taisei-yokoshima'}>linkedin.com/in/taisei-yokoshima</Div>
+   </div>;
+}
+
+function GitHub() {
+   return <div className="mt-[5rem]!">
+     <Header>GitHub</Header>
+     <Div href={'github.com/TaiseiYokoshima'}>github.com/TaiseiYokoshima</Div>
    </div>;
 }
 
 export default function Contact() {
-   return <>
+   return <div className="ml-[20rem]!">
       <Email/>
       <LinkedIn/>
-   </>;
+      <GitHub/>
+   </div>;
 }
-
