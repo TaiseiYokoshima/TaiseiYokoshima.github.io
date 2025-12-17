9272a164b5caf18100ad770e3085f2d2bd85fc8a
Taisei Yokoshima <taiseiyokoshima030712@gmail.com>
2025-12-17 18:30:45 +0000

    fixed wrong dates

--- src/Pages/Education/Education.tsx
+++ src/Pages/Education/Education.tsx
@@ -6,7 +6,7 @@ function Sussex() {
          <Header>University of Sussex</Header>
          <Div>Brighton, UK</Div>
       </div>
-      <Div>Sep  2022 - May 2024</Div>
+      <Div>Sep 2022 - May 2024</Div>
       <div className="flex justify-between">
          <Div>BSc Computer Science</Div>
          <Div>68% | 2:1 </Div>
@@ -14,24 +14,22 @@ function Sussex() {
    </div>;
 }
 
-
 function UWC() {
    return <div className="mt-[8rem]! w-full!">
       <div className="flex justify-between w-full!">
          <Header>UWC USA</Header>
          <Div>New Mexico, USA</Div> 
       </div>
-      <Div>Sep  2022 - May 2024</Div>
+      <Div>Aug 2019 - May 2021</Div>
       <Div>IB Diploma</Div>
       <Div>HL: Economics, Visual Arts, Physics</Div>
       <Div>SL: English, Italian, Math</Div>
    </div>;
 }
 
-
 export default function Education() {
-   return <>
+   return <div className="mx-[10rem]!">
       <Sussex/>
       <UWC/>
-   </>;
+   </div>;
 }
