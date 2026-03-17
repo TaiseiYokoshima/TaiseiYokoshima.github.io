baa8d98b43e2eea3cd16f55c1bd52428f289cf10
Taisei Yokoshima <taiseiyokoshima030712@gmail.com>
2026-03-17 19:59:03 +0700

    now settings open and close with the new navbar

--- src/NavBar/Page.tsx
+++ src/NavBar/Page.tsx
@@ -3,6 +3,9 @@ import styles from "./NavBar.module.css";
 import { useState } from "react";
 
 
+import Settings from "./Settings.tsx";
+
+
 function MenuOpener({ opener }: { opener: () => void }) {
    return <div className={styles.opener}>
       <div role="button" onClick={opener} className={styles.menuText}>»</div>
@@ -10,24 +13,36 @@ function MenuOpener({ opener }: { opener: () => void }) {
 }
 
 
-
 export default function NavPage() {
    const [ opened, set ] = useState(false);
-
-   const opener = () => set(true);
+   const open = () => {
+      // console.warn("menu opened");
+      set(true);
+   };
    const close = () => set(false);
+
+
+   const [settingsOpened, setSettings] = useState(false);
+   const openSettings = () => {
+      console.warn("settings opened");
+      setSettings(true);
+   };
+   const closeSettings = () => setSettings(false);
+
 
    return <>
       <div className={`${styles.page} terminal`} style={{ display: ( opened ? 'flex' : 'none') }}>
-         <div onClick={close} style={{ position: 'fixed', fontSize: '2rem', color: 'white', zIndex: '101', top: '0', left: '0'}}>X</div>
          <div>
+            <div onClick={close} className={styles.closer}>✕</div>
             <div className={styles.test} style={{ fontSize: 'var(--l)'}}>About</div>
             <div className={styles.test} style={{ fontSize: 'var(--l)'}}>Projects</div>
             <div className={styles.test} style={{ fontSize: 'var(--l)'}}>Experience</div>
             <div className={styles.test} style={{ fontSize: 'var(--l)'}}>Education</div>
             <div className={styles.test} style={{ fontSize: 'var(--l)'}}>Contact</div>
+            <div onClick={openSettings} className={styles.settingsOpener}>⚙</div>
          </div>
       </div>
-      <MenuOpener opener={opener}/>
+      <MenuOpener opener={open}/>
+      <Settings opened={settingsOpened} closeSettings={closeSettings}/>
    </>;
 }
--- src/NavBar/Settings.tsx
+++ src/NavBar/Settings.tsx
@@ -3,9 +3,11 @@ import "../App.css";
 
 import { useDispatch, useSelector } from "react-redux";
 import type { RootState } from "../store";
-import { toggleSettings, toggleAnimation, toggleDarkMode } from "../store";
-import { useEffect } from "react";
+import { toggleAnimation, toggleDarkMode } from "../store";
 import React from "react";
+
+
+import styles from "./Settings.module.css";
 
 
 function Switch({ enabled, callback, h='20px', w='30px', style={}, }: { enabled: boolean, callback: () => void, h?: string, w?: string, style?: React.CSSProperties }) {
@@ -36,57 +38,25 @@ function Setting({ callback, children, enabled }: { callback: () => void, childr
 }
 
 
-export default function Settings({ scheduleClose }: { scheduleClose: () => void }) {
-   const settingsOpen = useSelector((state: RootState) => state.app.settingsOpened);
+export default function Settings({ opened, closeSettings }: { opened: boolean, closeSettings: () => void }) {
+
+   console.warn("settings open state: ", opened);
+
+
    const animationEnabled = useSelector((state: RootState) => state.app.animationEnabled);
    const darkModeEnabled = useSelector((state: RootState) => state.app.darkModeEnabled);
    const dispatch = useDispatch();
    const flipAnimation = () => dispatch(toggleAnimation());
    const flipDarkMoode = () => dispatch(toggleDarkMode());
 
-   const closeSettings = () => {
-      dispatch(toggleSettings());
-   };
-
-   useEffect(() => {
-      if (!settingsOpen) return;
-      scheduleClose();
-   }, [settingsOpen]);
-
    return <>
-      {settingsOpen ?
-         <>
-            <div className="terminal" style={{
-               position: 'absolute',
-               top: "50%",
-               left: "50%",
-               transform: 'translate(-50%, -50%)',
-               width: '50vw',
-               height: '50vh',
-               zIndex: '10',
-               boxShadow: 'inset 0 0 0 0.2rem white'
-            }}>
-               <div style={{fontSize: '45px', textAlign: 'center', marginBottom: '2vh'}}>Preferences</div><div>
-                  <div role="button" onClick={closeSettings} style={{
-                        position: 'absolute',
-                        right: '0',
-                        top: '0',
-                        cursor: 'pointer',
-                        backgroundColor: 'white',
-                        color: 'black',
-                        width: '1.8em',
-                        height: '1.8em',
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        textAlign: 'center',
-                     }}>X</div>
-                  <Setting callback={flipAnimation} enabled={animationEnabled}>Animation | </Setting>
-                  <Setting callback={flipDarkMoode} enabled={darkModeEnabled}>Dark Mode | </Setting>
-               </div>
-            </div>
-            <div role="button" onClick={closeSettings} style={{position: 'absolute', top: '0', left: '0', width: '100%', height: '100%', zIndex: '2'}}/> 
-         </>
-         : null}
+      <div className={`terminal ${styles.container}`} style={{ display: (opened ? 'block' : 'none') }}>
+         <div className={styles.closer} role="button" onClick={closeSettings}>X</div>
+         <div style={{ fontSize: '45px', textAlign: 'center', marginBottom: '2vh'}}>Preferences</div><div>
+            <Setting callback={flipAnimation} enabled={animationEnabled}>Animation | </Setting>
+            <Setting callback={flipDarkMoode} enabled={darkModeEnabled}>Dark Mode | </Setting>
+         </div>
+      </div>
+      <div role="button" className={styles.catcher} onClick={closeSettings} style={{ display: (opened ? 'block' : 'none')  }}/> 
    </>;
 }
